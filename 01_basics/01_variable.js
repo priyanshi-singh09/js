9ed89beba4e6d5e0d8b353172dbb5dc5dd7e3d8a
priyanshi-singh09 <ps3109809@gmail.com>
2026-04-02 10:54:51 +0000

    variable and datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "ps31098@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur" 
+let accountState;
+/*
+not use var because of scope
+ */
+// accountId = 2 // not allowed
+accountEmail ="priyanshi@gmail.com"
+accountPassword = "989898"
+accountcity = "bengaluru"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
