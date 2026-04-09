e11dc502c903815bc80d502b439351bb972c318e
priyanshi-singh09 <ps3109809@gmail.com>
2026-04-09 10:28:03 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,18 @@
 "use strict"; // treat all JS Code as newer version
 // alert( 3 + 3 ) // we are using node js not browser
 console.log(3+3)
-console.log()
+
+
+// number => 2^53
+//bigint
+//string =>""
+// null => value
+// undefined => valur not assingned
+//symbol=>uniqueness
+let age =18
+let isloggedIn = false
+let state;
+/*object
+*/
+console.log(typeof null);
+console.log(typeof age)
