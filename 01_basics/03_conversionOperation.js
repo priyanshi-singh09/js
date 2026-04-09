59bbe7cdcea06095b742da5b6b59461fecdc815b
priyanshi-singh09 <ps3109809@gmail.com>
2026-04-09 11:02:55 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+// let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//output
+// number
+// number
+// number
+// 33
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// string
+// string
+// number
+// 33
+
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// string
+// string
+// number
+// NaN (not number)
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// /**
+//  * 1 => true; 0=> false
+//  * "" => false
+//  * "hitesh" => true
+//  */
+
+let someNumber =33
+let stringNumber= string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
