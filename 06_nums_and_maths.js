ec7a88612eefa7363f5e4f5855a1c605907cafa1
priyanshi-singh09 <ps3109809@gmail.com>
2026-06-01 06:17:22 +0000

    numbers

---
+++ 06_nums_and_maths.js
@@ -0,0 +1,16 @@
+const score = 4000
+console.log(score);  //4000
+
+const balance = new Number(100)
+    console.log(balance); //[Number: 100]
+  console.log(balance.toString().length);
+  console.log(balance.toFixed(2));
+
+
+  const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));  // gives rounded value with 4 digits 123.9
+
+const hundereds = 10000000
+console.log(hundereds.toLocaleString('en-IN'));
+
+///++++++++++++++++ Maths ++++++++++++++++++++++++
