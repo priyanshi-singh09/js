f7caf1c170c4f167189c72de9c76770d6105836a
priyanshi-singh09 <ps3109809@gmail.com>
2026-06-03 09:51:00 +0000

    maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,16 +1,34 @@
-const score = 4000
-console.log(score);  //4000
+// const score = 4000
+// console.log(score);  //4000
 
-const balance = new Number(100)
-    console.log(balance); //[Number: 100]
-  console.log(balance.toString().length);
-  console.log(balance.toFixed(2));
+// const balance = new Number(100)
+//     console.log(balance); //[Number: 100]
+//   console.log(balance.toString().length);
+//   console.log(balance.toFixed(2));
 
 
-  const otherNumber = 123.8966
-console.log(otherNumber.toPrecision(4));  // gives rounded value with 4 digits 123.9
+//   const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));  // gives rounded value with 4 digits 123.9
 
-const hundereds = 10000000
-console.log(hundereds.toLocaleString('en-IN'));
+// const hundereds = 10000000
+// console.log(hundereds.toLocaleString('en-IN'));
 
 ///++++++++++++++++ Maths ++++++++++++++++++++++++
+// console.log(Math); //Object [Math] {}
+// console.log(Math.round(4.6)); //5
+// console.log(Math.ceil(4.2)); //5
+// console.log(Math.floor(4.9));//4
+
+
+console.log(Math.random());// 0 to 1
+console.log((Math.random()*10) + 1); // 10 tk
+
+console.log(Math.floor(Math.random()*10) + 1); // more than one
+const min = 10
+const max =20
+
+
+
+
+
+
